27f455ccda51d625ebf1c20884105a42d0faac45
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-30 00:22:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function nameToAttributes(arr) {
   
   return arr.map(function (el) {
     el.split(" ")
+      debugger; 
       return Object.assign({}, {firstName: el[0], lastName: el[1]})
   })
 }
